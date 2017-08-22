085e8d1560d86221dde90fbbd3f0787d68849a1e
yuvarajac <yuva.jeeva@gmail.com>
2017-08-23 00:26:31 +0800

    add

---
+++ DotNet2017/WinConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello.. from \"Windows\" console app!");
+            Console.ReadLine();
+        }
+    }
+}
