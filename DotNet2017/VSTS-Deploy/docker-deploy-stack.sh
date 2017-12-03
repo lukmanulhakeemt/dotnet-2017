38b6b4174be52512edb91ff847816ce901f6aad7
NileshGule <vn_nilesh@yahoo.com>
2017-12-03 20:05:14 +0800

    tryingwith multiline commands

--- DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
+++ DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-export DOCKER_HOST=:2375  && cd deploy && docker stack deploy -c docker-compose.azure.yml webapp --with-registry-auth
+#export DOCKER_HOST=:2375  && cd deploy && docker stack deploy -c docker-compose.azure.yml webapp --with-registry-auth
+
+export DOCKER_HOST=:2375  
+cd deploy 
+docker stack deploy -c docker-compose.azure.yml webapp --with-registry-auth
