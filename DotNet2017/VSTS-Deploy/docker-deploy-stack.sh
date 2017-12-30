e5f6637b6c0e7173a73661d8618231b1ecd96176
NileshGule <vn_nilesh@yahoo.com>
2017-12-30 13:33:59 +0800

    updated usgae of env before calling docker stack deloy

--- DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
+++ DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 export DOCKER_HOST=:2375  
-export BUILD_BUILDID=$1
 cd deploy 
-docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
+env BUILD_BUILDID=$1 docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
