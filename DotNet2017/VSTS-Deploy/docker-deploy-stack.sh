9f15a39cf28d9d3cde11e386d9e13dcd1ed3aa32
NileshGule <vn_nilesh@yahoo.com>
2018-01-01 21:40:56 +0800

    trying to redeloy without build number

--- DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
+++ DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
@@ -2,5 +2,6 @@
 
 export DOCKER_HOST=:2375  
 cd deploy 
-echo $1
-env BUILD_BUILDID=$1 docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
+#echo $1
+#env BUILD_BUILDID=$1 docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
+env docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
