0bcce1a75ee25e42217fb69228a2ebb421421afd
NileshGule <vn_nilesh@yahoo.com>
2018-01-25 00:57:10 +0800

    using the build arg parameter

--- DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
+++ DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
@@ -3,5 +3,5 @@
 export DOCKER_HOST=:2375  
 cd deploy 
 #echo $1
-#env BUILD_BUILDID=$1 docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
-env docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
+env BUILD_BUILDID=$1 docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
+# env docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
