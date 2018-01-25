20b2c538cb72bb8526becb7c9ac0fde7b20d786f
NileshGule <vn_nilesh@yahoo.com>
2018-01-26 02:13:46 +0800

    combined 2 deploy scripts into 1

--- DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
+++ DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 export DOCKER_HOST=:2375  
+
+echo $2 | docker login -u $1 --password-stdin 
+
 cd deploy 
 #echo $1
-env BUILD_BUILDID=$1 docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
+env BUILD_BUILDID=$3 docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
 # env docker stack deploy -c docker-stack-swarm.yml webapp --with-registry-auth
