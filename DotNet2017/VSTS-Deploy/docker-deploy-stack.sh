f440f7121e004a05db4648e647b539571dbbf1b7
NileshGule <vn_nilesh@yahoo.com>
2018-01-29 23:26:28 +0800

    reformatted code, removed commented lines

--- DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
+++ DotNet2017/VSTS-Deploy/docker-deploy-stack.sh
@@ -5,6 +5,5 @@ export DOCKER_HOST=:2375
 echo $2 | docker login -u $1 --password-stdin 
 
 cd deploy 
-#echo $1
+
 env BUILD_BUILDID=$3 docker stack deploy --compose-file docker-compose-deploy.yml --prune webapp --with-registry-auth
-# env docker stack deploy --compose-file docker-swarm-deploy.yml webapp --with-registry-auth
