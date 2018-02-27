9977802df8dac116c04a0ffd9aa67255eeae7127
NileshGule <vn_nilesh@yahoo.com>
2018-02-28 00:21:33 +0800

    reduced wait time in initialization script to 10 seconds

--- DotNet2017/Database/setup-database.sh
+++ DotNet2017/Database/setup-database.sh
@@ -1,5 +1,5 @@
 echo 'Please wait while SQL Server 2017 warms up'
-sleep 30s
+sleep 10s
 
 echo 'Initializing database after 30 seconds of wait'
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyC0m9l&xP@ssw0rd -d master -i initialize-database.sql
